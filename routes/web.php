4eaabdaaf3f5d32ebb939de859bc428b786d201f
emma2625 <emmanuelodobo10@gmail.com>
2024-07-23 14:30:58 +0100

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,3 +11,6 @@ Route::get('/', [PagesController::class, 'home_page'])->name('home.page');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('post/create', [PostController::class, 'create'])->name('post.create');
