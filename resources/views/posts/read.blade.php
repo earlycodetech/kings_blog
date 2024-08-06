671b27e0d921c9344d0f8567077063f34fddf13a
emma2625 <emmanuelodobo10@gmail.com>
2024-08-06 14:10:17 +0100

    Updated Comment View

--- resources/views/posts/read.blade.php
+++ resources/views/posts/read.blade.php
@@ -10,11 +10,48 @@
                     </p>
                 </div>
 
-                <img src="{{ asset('uploads/'. $post->cover) }}" alt="{{ $post->title }}" class="w-100 post_cover">
+                <img src="{{ asset('uploads/' . $post->cover) }}" alt="{{ $post->title }}" class="w-100 post_cover">
 
                 <article class="my-4 card-body">
                     {!! $post->content !!}
                 </article>
+            </div>
+
+            <div class="card shadow border-0 my-5 mx-auto" style="max-width: 780px;">
+                <div class="card-header">
+                    <form action="" method="POST">
+                        @csrf
+
+                        <div class="mb-3">
+                            <label for="" class="form-label">Full name</label>
+                            <input name="name" id="" rows="5" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">Write Comment</label>
+                            <textarea name="comment" id="" rows="5" class="form-control"></textarea>
+                        </div>
+
+                        <div class="mb-3">
+                            <button class="btn btn-primary" > Save</button>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="card-body">
+
+                    <h4>Comments</h4>
+                    <!-- Some borders are removed -->
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <p class="">John Doe -  <small>3 Seconds </small></p>
+
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, recusandae.
+                            </p>
+                        </li>
+                    </ul>
+
+                </div>
             </div>
         </div>
     </section>
