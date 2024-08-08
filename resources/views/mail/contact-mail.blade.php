30111365cc524d6e09de041f2262cd084f043e0d
emma2625 <emmanuelodobo10@gmail.com>
2024-08-08 15:24:40 +0100

    Last Class Commit

--- resources/views/mail/contact-mail.blade.php
+++ resources/views/mail/contact-mail.blade.php
@@ -1,12 +1,25 @@
-<x-mail::message>
-# Introduction
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+    <div>
+        <h1>Name: {{ $data['name'] }}</h1>
 
-The body of your message.
+        <br><br>
+        <h1>Email: {{ $data['email'] }}</h1>
 
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
+        <br><br>
+        <h4>Message:</h4> <br>
 
-Thanks,<br>
-{{ config('app.name') }}
-</x-mail::message>
+        <p>
+            {{ $data['message'] }}
+        </p>
+
+    </div>
+</body>
+</html>
